f20575e1079bb2d0bd5ff2cb258720603f894f16
aawait1234 <110797730+aawait1234@users.noreply.github.com>
2023-02-26 16:07:44 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         jquery import
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Aaron
+// @match        https://www.mountainproject.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mountainproject.com
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js
+// ==/UserScript==
+
+
+
